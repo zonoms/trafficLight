ed161d15b8c83b5d0e2014dc10302584202f2d60
zon <zon_click@mail.ru>
2020-05-20 21:15:56 +0300

    Initial Commit

---
+++ trafficLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trafficLight
+//
+//  Created by ADMIN on 20.05.2020.
+//  Copyright © 2020 Arkadij Ryabov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
